1730ef79a9c1bd2822b9884f71d4fc951beecde7
Arcadier <AccountsPayable@arcadier.com>
2019-04-06 03:27:07 +0800

    Add files via upload

---
+++ user/scripts/scripts.js
@@ -0,0 +1,25 @@
+(function() {
+    var scriptSrc = document.currentScript.src;
+    var packagePath = scriptSrc.replace('/scripts/scripts.js', '').trim();
+    var re = /([a-f0-9]{8}(?:-[a-f0-9]{4}){3}-[a-f0-9]{12})/i;
+    var packageId = re.exec(scriptSrc.toLowerCase())[1];
+    var hostname = window.location.hostname;
+
+  if($('#merchantId') && $('#merchantId').length){
+    return 0;
+  } 
+  else {
+    if($('#userId') && $('#userId').length){
+    	var a = document.createElement("a");
+			a.href = "https://" + hostname + "/user/plugins/" + packageId + "/link.html";
+			a.innerHTML = "Link";
+			a.target = "_blank";
+
+		var b = document.createElement("li");
+			b.appendChild(a);
+
+		var c = document.querySelector("ul.navigation");
+			c.insertBefore(b, document.querySelector(".dropdown"));
+    }
+  }
+})();
