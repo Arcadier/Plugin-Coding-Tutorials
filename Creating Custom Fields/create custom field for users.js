d640d619c0c989c6b1fe106285f1b8faa4f393b8
Arcadier <AccountsPayable@arcadier.com>
2019-04-25 17:49:30 +0800

    Update create custom field for users.js

--- Creating Custom Fields/create custom field for users.js
+++ Creating Custom Fields/create custom field for users.js
@@ -1,72 +1,70 @@
+var baseURL = window.location.hostname;
+var token;
 
-    
-    var baseURL = window.location.hostname;
-    var token;
-    
-    //this token API isn't recommended to be used in JavaScript because it exposes the client ID and secret
-    var settings3 = {
-        "url": "https://" + baseURL + "/token",
+//this token API isn't recommended to be used in JavaScript because it exposes the client ID and secret
+var settings3 = {
+    "url": "https://" + baseURL + "/token",
+    "method": "POST",
+    "headers": {
+        "Content-Type": "application/x-www-form-urlencoded"
+    },
+    "data": {
+        "client_id": "{{client_id}}",
+        "client_secret": "{{client_secret}}",
+        "grant_type": "client_credentials",
+        "scope": "admin"
+    }
+};
+$.ajax(settings3).done(function(admin_token) {
+    token = "Bearer " + JSON.stringify(admin_token.access_token).replace(/"/g, '');
+    adminID = admin_token.UserId;
+
+    //creating JSON body for API
+    var user_data = {
+        "Name": "User Custom Field",
+        "IsMandatory": !1,
+        "DataInputType": "textfield",
+        "ReferenceTable": "Users",
+        "DataFieldType": "string",
+        "IsSearchable": !0,
+        "IsSensitive": !1,
+        "Active": !0
+    };
+
+    //calling Create Custom Field API
+    var settings2 = {
+        "url": "https://" + baseURL + "/api/v2/admins/" + adminID + "/custom-field-definitions",
         "method": "POST",
         "headers": {
-            "Content-Type": "application/x-www-form-urlencoded"
+            "Content-Type": "application/json",
+            "Authorization": token
         },
-        "data": {
-            "client_id": "{{client_id}}",
-            "client_secret": "{{client_secret}}",
-            "grant_type": "client_credentials",
-            "scope": "admin"
-        }
+        "data": JSON.stringify(user_data)
     };
-    $.ajax(settings3).done(function(admin_token) {
-        token = "Bearer " + JSON.stringify(admin_token.access_token).replace(/"/g, '');
-        adminID = admin_token.UserId;
-        
-        //creating JSON body for API
-        var user_data = {
-            "Name": "User Custom Field",
-            "IsMandatory": !1,
-            "DataInputType": "textfield",
-            "ReferenceTable": "Users",
-            "DataFieldType": "string",
-            "IsSearchable": !0,
-            "IsSensitive": !1,
-            "Active": !0
+    $.ajax(settings2).done(function(response) {
+        creation_response = response;
+        var userID = //userID of the user you want this value to be stored for
+
+        //CReate JSON body for Update User Info API
+        var data = {
+            "ID": userID,
+            "CustomFields": [{
+                "Code": creation_response.Code,
+                "Values": [value]
+            }]
         };
-        
-        //calling Create Custom Field API
-        var settings2 = {
-            "url": "https://" + baseURL + "/api/v2/admins/" + adminID + "/custom-field-definitions",
-            "method": "POST",
+        //call the Update User Information API
+        var settings = {
+            "url": "https://" + baseURL + "/api/v2/users/" + userID + "/",
+            "method": "PUT",
             "headers": {
                 "Content-Type": "application/json",
                 "Authorization": token
             },
-            "data": JSON.stringify(user_data)
+            "data": JSON.stringify(data),
         };
-        $.ajax(settings2).done(function(response) {
-            creation_response = response;
-            var userID = //userID of the user you want this value to be stored for
-            
-            //CReate JSON body for Update User Info API
-            var data = {
-                "ID": userID,
-                "CustomFields": [{
-                    "Code": creation_response.Code,
-                    "Values": [value]
-                }]
-            };
-            //call the Update User Information API
-            var settings = {
-                "url": "https://" + baseURL + "/api/v2/users/" + userID + "/",
-                "method": "PUT",
-                "headers": {
-                    "Content-Type": "application/json",
-                    "Authorization": token
-                },
-                "data": JSON.stringify(data),
-            };
-            $.ajax(settings).done(function(result) {
-                toastr.success("Success", "Great"); //the user now has a custom field with a value (line 54) stored in it
-            })
+        $.ajax(settings).done(function(result) {
+            toastr.success("Success", "Great"); //the user now has a custom field with a value (line 54) stored in it
         })
     })
+})
